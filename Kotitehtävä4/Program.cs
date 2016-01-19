e1969b0c46fa09f2dd5c680e65af4e40ae1a91ec
Jusqu123 <jusquu123@gmail.com>
2016-01-19 11:37:29 +0200

    kotitehtävä 1 (demo3teht4)

---
+++ Kotitehtävä4/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarApplication
+{
+    class Car
+    {
+        // fied member
+
+        //properties
+        public string Name { get; set; }
+        public int Speed { get; set; }
+
+        public int Tyres { get; set; }
+
+
+
+        // method displays car data
+        public void PrintData()
+        {
+            Console.WriteLine("Car Data: ");
+            Console.WriteLine("- name : " + Name);
+            Console.WriteLine("- Tyres : " + Tyres);
+            Console.WriteLine("- speed : " + Speed);
+
+        }
+        // method returns heater data
+        public override string ToString()
+        {
+            return "Name : " + Name + "tyres : " + Tyres + "speed : " + Speed;
+
+        }
+    }
+}
+
---
+++ Kotitehtävä4/car.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarApplication
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // create one car instance
+            Car car = new Car();
+            car.Name = "Audi";
+            car.Tyres = 240;
+            car.Speed = 100;
+
+
+            car.PrintData();
+            Console.WriteLine(car.ToString());
+            Console.ReadLine();
+        }
+    }
+}
+
+
+
+
+
+
